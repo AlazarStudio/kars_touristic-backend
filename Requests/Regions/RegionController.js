0a364706f3bb119b96f795894cdce39e25cf80f5
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-04-25 11:26:49 +0300

    Update

--- Requests/Regions/RegionController.js
+++ Requests/Regions/RegionController.js
@@ -6,16 +6,21 @@ class RegionController {
         try {
             const { title, description } = req.body;
             
+            if (!req.files || !req.files.iconPath || !req.files.backgroundImgPath) {
+                return res.status(400).send({ message: "Необходимо прикрепить файлы iconPath и backgroundImgPath" });
+            }
+    
             const iconPath = req.files.iconPath;
             const backgroundImgPath = req.files.backgroundImgPath;
-
+    
             const region = await RegionService.addRegion({ title, description, iconPath, backgroundImgPath });
-
+    
             res.status(201).send(region);
         } catch (error) {
             res.status(500).send({ message: "Не удалось добавить регион", error: error.message });
         }
-    };
+        
+    };  
 
 }
 
--- Requests/Regions/RegionService.js
+++ Requests/Regions/RegionService.js
@@ -3,17 +3,20 @@ import Region from "./Region.js"
 class RegionService {
 
     async addRegion(regionData) {
-        const { title, description} = regionData;
+        const { title, description, iconPath, backgroundImgPath } = regionData;
         
-        let iconPath = regionData.iconPath.name;
-        let backgroundImgPath = regionData.backgroundImgPath.name;
-
-        const region = new Region({ title, description, iconPath, backgroundImgPath });
-
+        if (!title || !description || !iconPath || !backgroundImgPath || !iconPath.name || !backgroundImgPath.name) {
+            throw new Error("Недостаточно данных для создания региона");
+        }
+    
+        let iconPathName = iconPath.name;
+        let backgroundImgPathName = backgroundImgPath.name;
+        
+        const region = new Region({ title, description, iconPath: iconPathName, backgroundImgPath: backgroundImgPathName });
         await region.save();
-
         return region;
     };
+    
 
 }
 
