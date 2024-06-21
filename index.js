96cbb46326b29976031e6c61d15d493c71aecf34
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-06-21 16:26:08 +0300

    Update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 app.use('/refs', express.static('static'));
-app.use(express.static(path.resolve(__dirname, '../kars_touristic/dist')));
+// app.use(express.static(path.resolve(__dirname, '../kars_touristic/dist')));
 
 // app.get('/*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../kars_touristic/dist', 'index.html'));
