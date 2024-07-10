5dd3b6be88a3ab0f86e4fae32db5b608ebc9db8a
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-07-10 16:37:19 +0300

    Update

--- Requests/Users/User.js
+++ Requests/Users/User.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const User = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    name: {type: String, required: true},
-    phone: {type: String, required: true},
-    email: {type: String, required: true},
-    likes: {type: Array, required: true, default: []},
-    cart: {type: Array, required: true, default: []},
-    role: {type: String, required: true, default: "user"},
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+    likes: { type: Array, required: true, default: [] },
+    cart: { type: Array, required: true, default: [] },
+    role: { type: String, required: true, default: "user" },
+    accept: { type: Boolean, required: true, default: false }
 }, { timestamps: true })
 
 export default mongoose.model('Users', User)
