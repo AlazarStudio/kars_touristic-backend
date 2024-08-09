3d6feb49b02494adbeaae5663256097d39ce35d2
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-08-09 14:49:03 +0300

    Update

--- Requests/MultidayTour/MultidayTourController.js
+++ Requests/MultidayTour/MultidayTourController.js
@@ -15,7 +15,6 @@ class MultidayTourController {
                 days: body.days || [],
                 mainPhoto: photoPaths[0],
             };
-            console.log(multidayTourData)
             const tour = await MultidayTourService.multidayTour(multidayTourData);
             res.status(201).json(tour);
         } catch (error) {
