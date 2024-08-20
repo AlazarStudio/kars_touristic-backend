7a4c1d572cf4a4ec5f0a4e21529337ed97d193ee
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-08-20 19:29:17 +0300

    Update

--- Requests/Agent/Agent.js
+++ Requests/Agent/Agent.js
@@ -8,6 +8,7 @@ const Agent = new mongoose.Schema({
     tours: { type: Array, required: true },
     passengers: { type: Array, required: true },
     bookingDate: { type: Date, required: true },
+    bookingTime: { type: String, required: true },
     paymanetState: { type: String, required: true, default: 'processing' },
     confirm: { type: Boolean, required: true, default: false },
 }, { timestamps: true });
