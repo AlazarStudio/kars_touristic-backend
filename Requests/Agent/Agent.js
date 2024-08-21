bbc8bd3961a5ab34b68c9f603ad3aae013f9eca7
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-08-21 10:16:43 +0300

    Update

--- Requests/Agent/Agent.js
+++ Requests/Agent/Agent.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const Agent = new mongoose.Schema({
     price: { type: String, required: true },
     agent: { type: String, required: true },
-    paymentType: { type: String, required: true },
-    
+    bronTypeRole: { type: String, required: true },
+    paymentType: { type: String, required: true },    
     tours: { type: Array, required: true },
     passengers: { type: Array, required: true },
     bookingDate: { type: Date, required: true },
