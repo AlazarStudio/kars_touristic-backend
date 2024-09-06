12d306c431c25df67509edf2056415d7421e6e90
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-09-06 12:28:45 +0300

    Update

--- Requests/MultidayTour/MultidayTourService.js
+++ Requests/MultidayTour/MultidayTourService.js
@@ -12,19 +12,19 @@ class MultidayTourService {
 
     async dublicateMultidayTour(multidayTourData) {
         const { mainPhoto, photos, ...restData } = multidayTourData;
-    
+
         const newTour = new MultidayTour({
             ...restData,
             mainPhoto: '',
             photos: []
         });
-    
+
         if (mainPhoto) {
             // Используем this для вызова метода copyImage
             const newMainPhotoPath = await this.copyImage(mainPhoto);
             newTour.mainPhoto = newMainPhotoPath;
         }
-    
+
         if (photos && photos.length > 0) {
             for (let photoPath of photos) {
                 // Используем this для вызова метода copyImage
@@ -32,22 +32,26 @@ class MultidayTourService {
                 newTour.photos.push(newPhotoPath);
             }
         }
-    
+
         await newTour.save();
-    
+
         return newTour;
     }
-    
 
     async copyImage(imagePath) {
-        const imageDir = path.dirname(imagePath);
-        const imageExt = path.extname(imagePath);
-        const imageBaseName = path.basename(imagePath, imageExt);
+        const fullImagePath = path.resolve('static', imagePath); // Пример построения абсолютного пути
+        if (!fs.existsSync(fullImagePath)) {
+            throw new Error(`File not found: ${fullImagePath}`);
+        }
+
+        const imageDir = path.dirname(fullImagePath);
+        const imageExt = path.extname(fullImagePath);
+        const imageBaseName = path.basename(fullImagePath, imageExt);
 
         const newImageName = `${imageBaseName}_copy${Date.now()}${imageExt}`;
         const newImagePath = path.join(imageDir, newImageName);
 
-        await fs.promises.copyFile(imagePath, newImagePath);
+        await fs.promises.copyFile(fullImagePath, newImagePath);
 
         return newImagePath;
     }
